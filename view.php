d5b217af48d07927a03b097f34a7899aa07900cc
AL SABAB <sabab015@gmail.com>
2022-10-31 11:33:53 +0600

    view page added

---
+++ view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>View</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Header</h1>
+        <h2>Title</h2>
+        <h3>Shoulder</h3>
+    </div>
+    <div class="container">
+        <p>Details Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque nisi odit rem quis expedita repudiandae tempore iusto consequuntur, iste voluptate!</p>
+        <p>Brief Lorem ipsum dolor sit amet consectetur adipisicing elit. Non perferendis cumque quaerat reprehenderit quos laboriosam consequatur esse natus at, a minima nemo corrupti eaque laborum expedita, velit deserunt, culpa est!</p>
+        <p>Author Name</p>
+    </div>
+    <div class="container">
+        <img src="https://media.istockphoto.com/vectors/sample-sign-sample-square-speech-bubble-sample-vector-id1161352480?k=20&m=1161352480&s=612x612&w=0&h=uVaVErtcluXjUNbOuvGF2_sSib9dZejwh4w8CwJPc48=" alt="">
+        <p>Symbol Name</p>
+        <p>Photo Details</p>
+        <p>Tags</p>
+        <p>News Category</p>
+    </div>
+</body>
+
+</html>
